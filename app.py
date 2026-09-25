5845bab3d0236936b6d82aaf351212ae90632f0f
tanusingh-30 <tanusingh1027@gmail.com>
2026-09-25 23:54:37 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st 
-st.title('my first streamit app')
-st.header('welcome!')
-st.write('this is a basic app')
-name = st.text_input("arin")
-if name:
-    st.success(f'hey, {name}!')
